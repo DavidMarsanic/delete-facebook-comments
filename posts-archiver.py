7302962229f795cd01ba4e15fa8bf9bccb250d7c
David <marsanic.david@gmail.com>
2023-09-20 19:18:23 +0200

    managed to get the post archiver to work

--- posts-archiver.py
+++ posts-archiver.py
@@ -15,7 +15,7 @@ browser = webdriver.Chrome(options=options)
 # Navigate directly to the activity log for comments
 browser.get('https://web.facebook.com/1322687115/allactivity?activity_history=false&category_key=MANAGEPOSTSPHOTOSANDVIDEOS&manage_mode=false&should_load_landing_page=false')
 
-def select_and_remove():
+def select_and_archive():
     try:
         # Attempt to click the checkbox
         checkbox = browser.find_element(By.NAME, 'comet_activity_log_select_all_checkbox')
@@ -23,46 +23,36 @@ def select_and_remove():
             checkbox.click()
             print("Checkbox clicked successfully!")
             
-            # Click the "Remove" button
-            remove_button = browser.find_element(By.XPATH, "//span[text()='Trash']")
-            remove_button.click()
+            # Click the "Move to archive" button
+            archive_button = browser.find_element(By.XPATH, "//div[@aria-label='Archive'][@role='button']")
+            archive_button.click()
     except Exception as e:
         print("Error:", e)
 
-def final_confirmation():
+def confirm_archive():
     try:
-        confirm_button = browser.find_element(By.XPATH, "//div[@aria-label='Move to trash'][@role='button']")
-        confirm_button.click()
+        # Wait for the "Move to archive" button to be clickable
+        confirm_archive_button = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@aria-label='Move to archive'][@role='button']"))
+        )
+        confirm_archive_button.click()
+        print("Posts archived successfully!")
     except Exception as e:
-        print("Error during final confirmation:", e)
-
-def archive_button():
-    try:
-        archive_btn = browser.find_element(By.XPATH, "//div[@aria-label='Move to archive'][@role='button']")
-        archive_btn.click()
-    except Exception as e:
-        print("Error during archiving:", e)
-
-def archive_confirmation():
-    try:
-        confirm_archive_btn = browser.find_element(By.XPATH, "//div[@aria-label='Move to archive'][@role='button']")
-        confirm_archive_btn.click()
-    except Exception as e:
-        print("Error during archive confirmation:", e)
-
+        print("Error:", e)
+        
 while True:
-    select_and_remove()
+    select_and_archive()
     time.sleep(1)
     # Call the checkbox killer script
     subprocess.run([sys.executable, "/Users/david/Documents/Code/delete-facebook-comments/checkbox-killer"])
-    final_confirmation()
+    confirm_archive()
     
     # Continuously check if the first checkbox has become clickable every second
     while True:
         try:
             checkbox = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.NAME, 'comet_activity_log_select_all_checkbox')))
             if checkbox:
-                print("Checkbox is clickable again. Proceeding with the next deletion cycle.")
+                print("Checkbox is clickable again. Proceeding with the next archiving cycle.")
                 break
         except:
             print("Waiting for checkbox to become clickable...")
